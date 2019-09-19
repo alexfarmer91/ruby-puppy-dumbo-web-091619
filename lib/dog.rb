725ff37b3f4ffccd09bc27d8353e03c4e799eb95
Alex Farmer <farmale10@gmail.com>
2019-09-19 02:00:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,24 @@
 
 class Dog 
   
+  attr_accessor :name
+  
   @@all = []
   
  def initialize(name)
    @name = name 
    @@all.push(self)
+ end 
+ 
+ 
+ def self.all 
+   @@all 
+ end 
+ 
+ def self.print_all
+   @@all.map { |dog_instance|
+   self.name
+   
  end 
 
 
